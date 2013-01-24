ed71f8c7ea4bbcb1000421694f1a62ae5295b798
Jonas Ladenfors <jonas.ladenfors@leanon.se>
2013-01-24 20:55:30 +0100

    Testing directive->controller bindings, some progress.

--- web/js/zens-controller.js
+++ web/js/zens-controller.js
@@ -1,5 +1,5 @@
 function ZensGraf($scope) {
     $scope.isGraf = false;
-    $scope.title = 'Lorem Ipsum';
+    $scope.title = 'Some stuff';
     $scope.text = 'Neque porro quisquam est qui dolorem ipsum quia dolor...';
 }
--- web/js/zens-module.js
+++ web/js/zens-module.js
@@ -8,45 +8,11 @@ angular.module('zens', []).
             replace: true
         };
     }).
-
-    directive('zensnavItem', function(){
-        return {
-            restrict: 'E',
-            // This HTML will replace the zippy directive.
-            replace: true,
-            transclude: true,
-            scope: { title:'@zensItem=' },           
-            template: '<div>' +
-                '<div class="title">{{title}}</div>' +
-                '<div class="body" ng-transclude></div>' +
-                '</div>',
-            // The linking function will add behavior to the template
-            link: function(scope, element, attrs) {
-                // Title element
-                var title = angular.element(element.children()[0]),
-                // Opened / closed state
-                    opened = true;
-
-                // Clicking on title should open/close the zippy
-                title.bind('click', toggle);
-
-                // Toggle the closed/opened state
-                function toggle() {
-                    opened = !opened;
-                    element.removeClass(opened ? 'closed' : 'opened');
-                    element.addClass(opened ? 'opened' : 'closed');
-                }
-
-                // initialize the zippy
-                toggle();
-            }
-        }
-    }).
     directive('zensnav', function() {
         return {
             restrict: 'E',
             transclude: true,
-            scope: {},
+            scope: { title: '@titles'},
             templateUrl: '/web/partial/nav.html',
             replace: true
         };
