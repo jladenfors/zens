4f6e28e4afc32f136db58dd111c2251bb638d210
Jonas Ladenfors <jonas.ladenfors@leanon.se>
2013-02-27 22:27:51 +0100

    Cache support, through html5 localstorage

--- web/js/reader/zens-reader.js
+++ web/js/reader/zens-reader.js
@@ -11,77 +11,108 @@ function ZensReader(util,  $http, plotter) {
      */
     var sensor_e1 = function(domId) {
 
-        
-        $http.get("/getEl").success(
-            function(data) {
-                // Plot graf per day
-                plotter.plot(
-                    [util.calculateDelta(parseDataPerHour(data.res, util.elHandle))],
-                    $(util.idCreator(domId, "Day")),
-                    ["Kw/h"],
-                    [1, "hour"],
-                    "%H",
-                    util.zensTimeHash.today,
-                    util.zensTimeHash.tomorrow,
-                    0,
-                    10,
-                    "#F999000");
-                
-                // Plot graf per month
-                plotter.plot(
-                    [util.calculateDayDelta(parseDataPerDay(data.res, util.elHandle))],
-                    $(util.idCreator(domId, "Month")),
-                    ["Kw/h"],
-                    [1, "day"],
-                    "%d",
-                    util.zensTimeHash.firstDayOfMonth,
-                    util.zensTimeHash.nextMonth,
-                    40,
-                    120,
-                    "#111000");
-
-                /* Present daily price 
-                 document.getElementById('price').innerHTML = "Compricer: " + data.price + " Kr/h";
-                 document.getElementById('currprice').innerHTML = "Totalt idag: " +  util.priceCalculus(data.price, elhourDelta).toPrecision(2) + " Kr";
-                 document.getElementById('monthprice').innerHTML = "Totalt denna m?nad: " +  util.priceCalculus(data.price, eldayDelta).toPrecision(6) + " Kr";
-                 */
-
+        if(supports_html5_storage){
+            $http.get("/getEl").success(function(data){
+                plotElectric(domId, data);
             });
+        }
+        else {
+            if (localStorage.getItem("elData") == undefined)
+            {
+                $http.get("/getEl").success(function(data){
+                    localStorage.setItem("elData", JSON.stringify(data));
+                    plotElectric(domId, data);
+                });
+            }else{
+                var dataRes = JSON.parse(localStorage.getItem("elData"));                
+                plotElectric(domId, dataRes);
+            }
+        }
     };
+
+    function plotElectric(domId, data){
+        // Plot graf per day
+        plotter.plot(
+            [util.calculateDelta(parseDataPerHour(data.res, util.elHandle))],
+            $(util.idCreator(domId, "Day")),
+            ["Kw/h"],
+            [1, "hour"],
+            "%H",
+            util.zensTimeHash.today,
+            util.zensTimeHash.tomorrow,
+            0,
+            10,
+            "#F999000");
+
+        // Plot graf per month
+        plotter.plot(
+            [util.calculateDayDelta(parseDataPerDay(data.res, util.elHandle))],
+            $(util.idCreator(domId, "Month")),
+            ["Kw/h"],
+            [1, "day"],
+            "%d",
+            util.zensTimeHash.firstDayOfMonth,
+            util.zensTimeHash.nextMonth,
+            40,
+            120,
+            "#111000");
+
+        /* Present daily price 
+         document.getElementById('price').innerHTML = "Compricer: " + data.price + " Kr/h";
+         document.getElementById('currprice').innerHTML = "Totalt idag: " +  util.priceCalculus(data.price, elhourDelta).toPrecision(2) + " Kr";
+         document.getElementById('monthprice').innerHTML = "Totalt denna m?nad: " +  util.priceCalculus(data.price, eldayDelta).toPrecision(6) + " Kr";
+         */
+    }
 
     /**
-    * Popluate sensor t1
-    * @param domId the dom to plot to, remember that the dom needs to be finished before plotting.
-    */
+     * Popluate sensor t1
+     * @param domId the dom to plot to, remember that the dom needs to be finished before plotting.
+     */
     var sensor_t1 = function(domId) {
 
-        $http.get("/getTemp").success(
-            function(data){
-                plotter.plot(
-                    [util.orderHashSets(parseDataPerHour(data, util.tempHandle))],
-                    $(util.idCreator(domId, "Day")),
-                    ["C"],
-                    [1, "hour"],
-                    "%H",
-                    util.zensTimeHash.today,
-                    util.zensTimeHash.tomorrow,
-                    10,
-                    30,
-                    '#fff000');
-                plotter.plot(
-                    [util.orderHashSets(parseDataPerDay(data, util.tempHandle))],
-                    $(util.idCreator(domId, "Month")),
-                    ["C"],
-                    [1, "day"],
-                    "%d",
-                    util.zensTimeHash.today,
-                    util.zensTimeHash.tomorrow,
-                    10,
-                    30,
-                    '#fff000');
+        if(supports_html5_storage){
+            $http.get("/getTemp").success(function(data){
+                plotTemperatureData(domId, data);
             });
+        }
+        else {
+            if (localStorage.getItem("t1Data") == undefined)
+            {
+                $http.get("/getTemp").success(function(data){
+                    localStorage.setItem("t1Data", JSON.stringify(data));
+                    plotTemperatureData(domId, data);
+                });
+            }else{
+                var dataRes = JSON.parse(localStorage.getItem("t1Data"));                
+                plotTemperatureData(domId, dataRes);
+            }
+        }
     };
 
+    function plotTemperatureData(domId, data){
+        plotter.plot(
+            [util.orderHashSets(parseDataPerHour(data, util.tempHandle))],
+            $(util.idCreator(domId, "Day")),
+            ["C"],
+            [1, "hour"],
+            "%H",
+            util.zensTimeHash.today,
+            util.zensTimeHash.tomorrow,
+            10,
+            30,
+            '#fff000');
+        plotter.plot(
+            [util.orderHashSets(parseDataPerDay(data, util.tempHandle))],
+            $(util.idCreator(domId, "Month")),
+            ["C"],
+            [1, "day"],
+            "%d",
+            util.zensTimeHash.today,
+            util.zensTimeHash.tomorrow,
+            10,
+            30,
+            '#fff000');
+    }
     var parseDataPerHour = function (data, dataHandle){
         var hourHash = {};
         data.forEach(
